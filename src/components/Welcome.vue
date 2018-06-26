32ffc4cd62e1029eb43e6921b12d6d6199154a6a
Miguel Torres <migueltg93@gmail.com>
2018-06-26 10:49:57 -0500

    Added Welcome & Chat components, navigation guards

---
+++ src/components/Welcome.vue
@@ -0,0 +1,50 @@
+<template>
+  <div class="welcome container">
+    <div class="card">
+      <div class="card-content center-align">
+        <h2 class="teal-text">Welcome</h2>
+        <form class="" @submit.prevent="enterChat">
+          <label for="name">Enter your Name</label>
+          <input type="text" name="name" v-model="name">
+          <p v-if="feedback" class="red-text">{{ feedback }}</p>
+          <button class="btn teal">Enter Chat</button>
+        </form>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Welcome',
+  data () {
+    return {
+      name: null,
+      feedback: null
+    }
+  },
+  methods: {
+    enterChat() {
+      if (this.name) {
+        this.$router.push({ name: 'Chat', params: { name: this.name } })
+      } else{
+        this.feedback = "You must enter a name to be able to chat"
+      }
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+div {
+  max-width: 400px;
+  margin-top: 100px;
+}
+h2{
+  font-size: 3em;
+}
+button{
+  margin: 30px auto;
+}
+</style>
