62f6a611ce523f78e6fa5c82eeb069e9c8fff01d
Miguel Torres <migueltg93@gmail.com>
2018-06-27 14:09:25 -0500

    Misc changes and deploy config

--- src/components/Chat.vue
+++ src/components/Chat.vue
@@ -3,7 +3,7 @@
     <h2 class="center teal-text">Chat</h2>
     <div class="card">
       <div class="card-content">
-        <ul class="messages">
+        <ul class="messages" v-chat-scroll>
           <li v-for="message in messages" :key="message.id">
             <span class="teal-text">{{ message.name }}</span>
             <span class="grey-text text-darken-3">{{ message.content }}</span>
@@ -21,6 +21,7 @@
 <script>
 import NewMessage from '@/components/NewMessage'
 import db from '@/firebase/init'
+import moment from 'moment'
 
 export default {
   name: 'Chat',
@@ -35,7 +36,7 @@ export default {
   },
   created() {
     let ref = db.collection('messages').orderBy('timestamp')
-    
+
     ref.onSnapshot(snapshot => {
       snapshot.docChanges().forEach(change => {
         if(change.type == 'added') {
@@ -44,7 +45,7 @@ export default {
             id: doc.id,
             name: doc.data().name,
             content: doc.data().content,
-            timestamp: doc.data().timestamp
+            timestamp: moment(doc.data().timestamp).format('lll')
           })
         }
       })
@@ -63,6 +64,19 @@ span{
 }
 .time{
   display: block;
-  font-size: 1.2em;
+  font-size: 0.8em;
+}
+.messages{
+  max-height: 300px;
+  overflow: auto;
+}
+.messages::-webkit-scrollbar{
+  width: 3px;
+}
+.messages::-webkit-scrollbar-track{
+  background: #ddd;
+}
+.messages::-webkit-scrollbar-thumb{
+  background: #aaa;
 }
 </style>
